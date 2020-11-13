92e00b6a3db6cb5c2a56a0c077cf599c33bca48c
106590022 <47914389+106590022@users.noreply.github.com>
2020-11-13 15:21:07 +0800

    final

--- src/views/about.vue
+++ src/views/about.vue
@@ -4,6 +4,25 @@
     <p class="title">ABOUT</p>
     <p class="line"></p>
   </b-row>
+  <b-row>
+    <b-col>
+      <b-img class="w-75" :src="require('../assets/about/aboutpic.png')"></b-img>
+    </b-col>
+    <b-col>
+    <p class="info_title">Company Profile</p>
+    <p class="info_text">Quantum Radiation Technology Co., Ltd. is a
+radiation safety professional company approved by
+the Atomic Energy Commission of the Executive
+Yuan and has obtained radiation protection
+detection, training services, and sales services at
+the same time; it is deeply involved in the domestic
+radiation protection and shielding design field
+    </p>
+    <div>
+      <button type="button" class=" more_but btn btn-outline-success float-right" >more</button>
+    </div>
+</b-col>
+  </b-row>
 </div>
 </template>
 
@@ -27,6 +46,31 @@ export default{
   width: 100%;
   height: 2px;
   background-color:#07833B;
+}
+
+.info_icon{
+  width: 30vw;
+  margin-left: 1vw;
+  
+}
+
+.info_text{
+  color: #707070;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.2vw;
+  text-align: left;
+}
+
+.info_title{
+  color: #707070;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.4vw;
+  text-align: left;
+}
+
+.btn-outline-danger{
+  color: #07833B;
+  border-color: #07833B;
 }
 </style>
 
--- src/views/service.vue
+++ src/views/service.vue
@@ -4,6 +4,78 @@
     <p class="title">SERVICE</p>
     <p class="line"></p>
   </b-row>
+  <b-row>
+    <b-col class="rounded info_back_right">
+      <b-row class="info-row" >
+        <b-col cols="6" class="">
+          <b-img class="info_icon align-items-center" :src="require('../assets/service/Group 4.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Radio Protection <br>Engineering Consultant &<br>Project Management</p>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col class="rounded info_back_left">
+      <b-row class="info-row">
+        <b-col cols="6">
+          <b-img class="info_icon" :src="require('../assets/service/Group 10.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Radio Protection Detect</p>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+  
+  <b-row>
+    <b-col>
+      <b-row class="info-row">
+        <b-col cols="6">
+          <b-img class="info_icon" :src="require('../assets/service/Group 11.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Radiation Source <br>Shipping / Scrap<br>/ Decontamination</p>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col>
+      <b-row class="info-row">
+        <b-col cols="6">
+          <b-img class="info_icon" :src="require('../assets/service/Group 12.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Medical Exposure <br>Quality Assurance Service
+</p>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+  <b-row>
+    <b-col>
+      <b-row class="info-row">
+        <b-col cols="6">
+          <b-img class="info_icon" :src="require('../assets/service/Group 16.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Radio Protection <br>Education Training</p>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col>
+      <b-row class="info-row">
+        <b-col cols="6">
+          <b-img class="info_icon" :src="require('../assets/service/Group 17.png')"></b-img>
+        </b-col>
+        <b-col cols="6">
+          <p class="serv_info">Sales</p>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+
+
+
+
 </div>
 </template>
 
@@ -26,8 +98,35 @@ export default{
 .line{
   width: 100%;
   height: 2px;
-  background-color:rgb(7, 131, 59, 0.4);
-  
+  background-color:rgb(7, 131, 59, 0.4); 
 }
+
+.info_icon{
+  width: 7vw;
+  margin-left: 1vw;
+  margin-bottom: 2.4vw;
+}
+.info-row{
+  border-radius:10px;
+  color:#07833B;
+}
+.serv_info{
+   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1.5vw;
+  color: #707070;
+  text-align: left;
+}
+
+/* .info_back_right{
+  background-color: rgb(150, 201, 171);
+  size: 97nx;
+  
+} */
+
+/* .info_back_left{
+  background-color: rgb(150, 201, 171);
+  size: 20nx;
+  margin-left: 3nx;
+} */
 </style>
 
